c87abe365401120f8569b8495e17ab516a0786c4
Crobbet <bitiferow@gmail.com>
2025-10-26 22:13:49 +0600

    Default

--- nixos/system.nix
+++ nixos/system.nix
@@ -96,7 +96,7 @@
     services.xserver.enable = true;
     services.xserver.desktopManager.xterm.enable = false;
     services.displayManager.sddm.enable = true;
-    services.desktopManager.cosmic.enable = true;
+    services.xserver.desktopManager.mate.enable = true;
     services.guix.enable = true;
 
     services.xserver.xkb = {
