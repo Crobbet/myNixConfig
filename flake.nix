d94b0394ab0fbc0551cf49b7f5a963b18d71822f
Crobbet <bitiferow@gmail.com>
2025-10-28 21:14:57 +0600

    Default

--- flake.nix
+++ flake.nix
@@ -10,10 +10,27 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
+    nvf.url = "github:notashelf/nvf";
+
   };
 
-  outputs = { self, nixpkgs, ... }@inputs:
+  outputs = { self, nixpkgs, nvf, ... }@inputs:
     {
+    packages.x86_64-linux.my-neovim =
+      (nvf.lib.neovimConfiguration {
+        pkgs = nixpkgs.legacyPackages.x86_64-linux;
+        modules = [
+        (
+            {pkgs, ...}: {
+             config.vim = {
+                theme.enable = true;
+              };
+            }
+          )
+        ];
+      })
+      .neovim;
+
      nixosConfigurations.Winter = nixpkgs.lib.nixosSystem {
       specialArgs = {inherit inputs;};
       modules = [
