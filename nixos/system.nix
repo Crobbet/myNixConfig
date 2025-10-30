04557287307f28651de5482404d98f435a0cf270
Crobbet <bitiferow@gmail.com>
2025-10-30 19:51:39 +0600

    Default

--- nixos/system.nix
+++ nixos/system.nix
@@ -117,4 +117,44 @@
       ExecStart = "${pkgs.bash}/bin/bash -c 'echo 1 > /sys/bus/usb/devices/1-0:1.0/usb1-port4/disable'";
     };
   };
+
+  environment.systemPackages = with pkgs; [
+    xfce.xfce4-panel
+    xfce.xfce4-panel-profiles
+
+    xfce.xfce4-appfinder
+    xfce.xfce4-battery-plugin
+
+    acpi
+    light
+
+    killall
+    haskell-language-server
+    xsettingsd
+
+    picom-pijulius
+    iosevka
+    nerd-fonts.iosevka
+  ];
+
+  services.xserver = {
+    desktopManager = {
+      xfce = {
+        enable = true;
+        noDesktop = true;
+        enableXfwm = false;
+      };
+    };
+    windowManager = {
+      xmonad = {
+        enable = true;
+        enableContribAndExtras = true;
+        extraPackages = haskellPackages: [
+          haskellPackages.xmonad-contrib
+          haskellPackages.xmonad-extras
+          haskellPackages.xmonad
+        ];
+      };
+    };
+  };
 }
