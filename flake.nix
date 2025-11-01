58a75207de428fc6ed85c19481eb8f4fc2082b2a
Crobbet <bitiferow@gmail.com>
2025-11-02 01:27:39 +0600

    Default

--- flake.nix
+++ flake.nix
@@ -47,7 +47,7 @@
 {
   stylix = {
     enable = true;
-    base16Scheme = "${pkgs.base16-schemes}/share/themes/g.yaml";
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
     };
 })
         ./nixos/system.nix
