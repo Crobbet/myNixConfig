747b523f6b8f95332d4eebb7f838380e6be00cab
Crobbet <bitiferow@gmail.com>
2025-11-01 22:41:28 +0600

    Default

--- nixos/system.nix
+++ nixos/system.nix
@@ -161,6 +161,6 @@
 
   stylix = {
     enable = true;
-    base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
+    #    base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
   };
 }
