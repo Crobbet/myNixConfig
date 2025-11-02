0956ed06991a38daa7fde49660ff0cfadd26c743
Crobbet <bitiferow@gmail.com>
2025-11-02 17:09:52 +0600

    Default

--- flake.nix
+++ flake.nix
@@ -47,7 +47,7 @@
           stylix = {
             enable = true;
             #    base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
-            image = ./nixos/wallpaper.png;
+            image = ./nixos/1379491.png;
             polarity = "dark";
           };
         })
