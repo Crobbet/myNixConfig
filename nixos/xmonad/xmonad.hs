7f9992f4f50dc7b2c2448f72b6a8669db1d7f5eb
Crobbet <bitiferow@gmail.com>
2025-11-03 17:12:06 +0600

    Default

--- nixos/xmonad/xmonad.hs
+++ nixos/xmonad/xmonad.hs
@@ -71,8 +71,8 @@ myKeys conf@(XConfig {XMonad.modMask = super}) = M.fromList $
 
     , ((super,                           xK_c),  kill)
     , ((super,                      xK_Escape),  spawn "xkill")
-    , ((super,                           xk_d), spawn "xfce4-appfinder -c --disable-server")
-    , ((super,                           xk_a)), spawn "xfce4-appfinder --disable-server"
+    , ((super,                           xK_d), spawn "xfce4-appfinder -c --disable-server")
+    , ((super,                           xK_a)), spawn "xfce4-appfinder --disable-server"
 
     -- Window Manager Specific -----------------------------------------
 
