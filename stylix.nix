d943ec0d4e3feb2e789c16b045c7fa8af4abd767
Crobbet <bitiferow@gmail.com>
2025-11-07 20:57:10 +0600

    Default

--- stylix.nix
+++ stylix.nix
@@ -4,26 +4,26 @@
     #    base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
     image = ./nixos/miku.jpg;
     polarity = "dark";
-    fonts = {
-      serif = {
-        package = pkgs.iosevka;
-        name = "Comic Sans";
-      };
-
-      sansSerif = {
-        package = pkgs.iosevka;
-        name = "Comic";
-      };
-
-      monospace = {
-        package = pkgs.nerd-fonts.hack;
-        name = "ComicShans";
-      };
-
-      emoji = {
-        package = pkgs.iosevka;
-        name = "Noto Color Emoji";
-      };
-    };
+#    fonts = {
+#      serif = {
+#        package = pkgs.iosevka;
+#        name = "Comic Sans";
+#      };
+#
+#      sansSerif = {
+#        package = pkgs.iosevka;
+#        name = "Comic";
+#      };
+#
+#      monospace = {
+#        package = pkgs.nerd-fonts.hack;
+#        name = "ComicShans";
+#      };
+#
+#      emoji = {
+#        package = pkgs.iosevka;
+#        name = "Noto Color Emoji";
+#      };
+#    };
   };
 }
