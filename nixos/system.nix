349e278eb3e613fee24e2d8d3dc7d8998dcbb975
Crobbet <bitiferow@gmail.com>
2025-11-29 13:09:34 +0600

    Default

--- nixos/system.nix
+++ nixos/system.nix
@@ -95,7 +95,7 @@
     enable = true;
   };
   services.gnome = {
-    core-apps.enable = false;
+    #core-apps.enable = false;
     core-developer-tools.enable = false;
     games.enable = false;
   };
