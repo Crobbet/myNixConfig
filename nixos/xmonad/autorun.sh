b955644ad2c25cab622373bac78bd528097c96f5
Crobbet <bitiferow@gmail.com>
2025-12-05 11:52:40 +0600

    Default

--- nixos/xmonad/autorun.sh
+++ nixos/xmonad/autorun.sh
@@ -4,5 +4,5 @@ nitrogen --restore &
 #xfce4-panel --disable-wm-check &
 picom --config /home/bt/.xmonad/picom.conf &
 #xfce4-power-manager &
-#xfce4-volumed-pulse &
+xfce4-volumed-pulse &
 #xfce4-session &
