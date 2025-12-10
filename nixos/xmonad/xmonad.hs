528b5f65ec9e1c7a8c996a78c81acda9e2b3d14e
Crobbet <bitiferow@gmail.com>
2025-12-10 17:01:20 +0600

    Default

--- nixos/xmonad/xmonad.hs
+++ nixos/xmonad/xmonad.hs
@@ -52,7 +52,7 @@ myManageHook = composeAll
     , resource  =? "desktop_window" --> doIgnore
     ]
 
-myBrightnessControls x do
+myBrightnessControls x = x do
     | x < 0 = "brightnessctl s -5%"
     | x > 0 = "brightnessctl s +5%"
     | x == 0 = "brightnessctl s 1%"
