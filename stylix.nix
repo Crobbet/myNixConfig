99fd696c4372d6b9d8db80486ed8f698862d1dc9
Crobbet <bitiferow@gmail.com>
2025-12-17 17:30:17 +0600

    Default

--- stylix.nix
+++ stylix.nix
@@ -2,7 +2,7 @@
   stylix = {
     enable = true;
     #    base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
-    image = ./b-330.jpg;
+    image = ./b-682.jpg;
     polarity = "dark";
   };
 }
